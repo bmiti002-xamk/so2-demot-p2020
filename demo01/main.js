7967113ecb9feaa3593edf8a93eec687abf3ba83
Mimmi Tick <63038379+bmiti002-xamk@users.noreply.github.com>
2020-08-31 12:12:23 +0300

    Update main.js
    Mimmin arvauksia...

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); //näistä ei ole tavallaan enää mitään käryä
+const url = require("url"); //eikä näistä
 
-const portti = 3001;
+const portti = 3001; //portin määrittely 
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //luodaan serveri
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { 
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; //muuttuja tiedot
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //muuttuja nimi
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //hmtl sivun headin määritykset eli tekstiä ja merkistökoodi
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //pyydetään serveriltä että vastaa kirjoittalla heippa ja nimen jonka saa nimi-muuttujasta
+        res.end(); //tällä lopetettiin jotain... 
 
     }
 
 });
-
+//käynnistetään serveri
 serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   //konsoli ilmoitus mustaan ruutuun
 });
